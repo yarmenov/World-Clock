aea214e87dc56eaf2c5486a8b385a331fe8b8c61
yana <yarmenova@gmail.com>
2023-05-13 14:29:52 +0100

    added multiple locations

--- javascript/index.js
+++ javascript/index.js
@@ -39,5 +39,26 @@ function showTime() {
     .tz("America/Los_Angeles")
     .format("HH:mm:ss[<small>] A [</small]");
 }
+
 showTime();
 setInterval(showTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+     <div class="time">${cityTime.format("HH:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+let citiesContent = document.querySelector("#city");
+citiesContent.addEventListener("change", updateCity);
